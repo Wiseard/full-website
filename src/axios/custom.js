3ce856baab237998d03d19cd8221cfe59dfb7af4
Florencio <florentbaleinier@gmail.com>
2023-08-02 23:43:53 +0200

    edit api token

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const authFetch = axios.create({
-  baseURL: `https://${import.meta.env.VITE_PUBLIC_SANITY_TOKEN}.api.sanity.io`,
-  // baseURL: 'https://2s86hdla.api.sanity.io',
+  // baseURL: `https://${import.meta.env.VITE_PUBLIC_SANITY_TOKEN}.api.sanity.io`,
+  baseURL: 'https://2s86hdla.api.sanity.io',
   headers: {
     Accept: 'application/json',
   },
